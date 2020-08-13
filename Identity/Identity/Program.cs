3bbdeba33470133d8e08411c2d3f7c77e016e7a8
Wenlin Huang <jackhwl@hotmail.com>
2020-08-13 16:40:06 -0400

    7. Demo- Verifying a Password

--- Identity/Identity/Program.cs
+++ Identity/Identity/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,22 @@ namespace Identity
     {
         static void Main(string[] args)
         {
+            var username = "huang@wenlin.net";
+            var password = "password";
+
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
 
-            var creationResult = userManager.Create(new IdentityUser("huang2@wenlin.net"), "password");
-            Console.WriteLine("Created: {0}", creationResult.Succeeded);
+            //var creationResult = userManager.Create(new IdentityUser(username), password);
+
+            var user = userManager.FindByName(username);
+            //var claimResult = userManager.AddClaim(user.Id, new Claim("given_name", "Huang"));
+
+            //Console.WriteLine("Claim: {0}", claimResult.Succeeded);
+            //Console.WriteLine("Created: {0}", creationResult.Succeeded);
+
+            var isMatch = userManager.CheckPassword(user, password);
+            Console.WriteLine("Password Match: {0}", isMatch);
             Console.ReadLine();
         }
     }
