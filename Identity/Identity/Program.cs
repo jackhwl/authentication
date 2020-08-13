657c2ce9198986cebf3e23facdb2a379a98f6fac
Wenlin Huang <jackhwl@hotmail.com>
2020-08-13 16:15:20 -0400

    5. Demo- Creating a User

---
+++ Identity/Identity/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var creationResult = userManager.Create(new IdentityUser("huang2@wenlin.net"), "password");
+            Console.WriteLine("Created: {0}", creationResult.Succeeded);
+            Console.ReadLine();
+        }
+    }
+}
