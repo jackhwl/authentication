022d0058d2bd70f9cff50528bedb80dd604bf8df
Wenlin Huang <jackhwl@hotmail.com>
2020-09-13 10:19:46 -0400

    2. Examining the Data Store

---
+++ Authentication and Authorization in ASP.NET Core/IdentityExploration/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExploration.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public DateTime CareerStarted { get;set;}
+    }
+}
