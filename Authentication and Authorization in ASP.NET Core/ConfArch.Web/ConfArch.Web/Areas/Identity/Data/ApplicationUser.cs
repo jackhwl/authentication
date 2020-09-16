37c9dac4e1cdf2fbd46184e1ce63b8fa60347b55
Wenlin Huang <jackhwl@hotmail.com>
2020-09-15 22:40:59 -0400

    5. Retrofitting Identity

---
+++ Authentication and Authorization in ASP.NET Core/ConfArch.Web/ConfArch.Web/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfArch.Web
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime CareerStartedDate { get; set; }
+        public string FullName { get; set; }
+    }
+}
